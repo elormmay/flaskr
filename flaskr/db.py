cc4264befc0e3aae0a83a942e5c9e7e97044a563
elormmay <elormmay@gmail.com>
2022-11-12 01:42:58 +0000

    initialized sqlite database

--- flaskr/db.py
+++ flaskr/db.py
@@ -19,3 +19,22 @@ def close_db(e=None):
 
     if db is not None:
         db.close()
+
+
+def init_db():
+    db = get_db()
+    with current_app.open_resource("schema.sql") as f:
+        db.executescript(f.read().decode("utf-8"))
+
+
+@click.command("init-db")
+def init_db_command():
+    """Clear the existing database and create new tables"""
+    click.echo("Initializing the database...")
+    init_db()
+    click.echo("Initialized the database.")
+
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
